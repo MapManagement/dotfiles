4902f3ffdce586ef716b773648fcc6e6afca634d
Jan <MapManagement@protonmail.com>
2022-09-07 23:31:49 +0200

    Replace bash script with Python script, add additional i3 config for multiple monitors

---
+++ setup_machine.py
@@ -0,0 +1,34 @@
+ansible_parameters = {
+    "is_desktop": False,
+    "external_storage": False,
+    "multi_monitor": False
+}
+
+def read_user_inputs():
+    is_desktop_text = "Is this a machine for virtualization purposes(y/n)?"
+    is_desktop_answer = input(is_desktop_text)
+    ansible_parameters["is_desktop"] = convert_user_input(is_desktop_answer)
+
+    external_storage_text = "Are directories for external storage needed(y/n)?"
+    external_storage_answer = input(external_storage_text)
+    ansible_parameters["external_storage"] = convert_user_input(external_storage_answer)
+
+    multi_monitor_text = "Are you going to use multiple monitors on this device(y/n)?"
+    multi_monitor_answer = input(multi_monitor_text)
+    ansible_parameters["multi_monitor"] = convert_user_input(multi_monitor_answer)
+
+def convert_user_input(answer: str) -> bool:
+    return answer == "y"
+
+def execute_ansible_playbook():
+    ansible_command = ["ansible-playbook", "-K"]
+
+    for key in ansible_parameters.keys():
+        if ansible_parameters["key"]:
+            parameter = f'{key}="yes"'
+            ansible_command.append("-e")
+            ansible_command.append(parameter)
+
+if __name__ == "__main__":
+    read_user_inputs()
+    execute_ansible_playbook()
