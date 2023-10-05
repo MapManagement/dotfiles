5ea83865fed87d983c831593578085cfc5623ebb
Jan <MapManagement@protonmail.com>
2023-10-05 21:19:59 +0200

    Neovim: remove keymap for git files

--- roles/neovim/files/after/plugin/telescope.lua
+++ roles/neovim/files/after/plugin/telescope.lua
@@ -3,9 +3,6 @@ local builtin = require('telescope.builtin')
 -- find file by string
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 
--- find git file by string
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-
 -- ?
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ")});
